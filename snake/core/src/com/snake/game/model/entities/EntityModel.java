45b43943610bfa67fd6adfaf2defb9c75cb33593
Ana Rita Norinho <ritanorinho98@hotmail.com>
2018-04-22 19:46:22 +0100

    Added basic functions to snake model and entity model

---
+++ snake/core/src/com/snake/game/model/entities/EntityModel.java
@@ -0,0 +1,36 @@
+package com.snake.game.model.entities;
+
+public abstract class EntityModel {
+    public enum ModelType {SNAKE,SQUARE,WALL,COIN,BALL};
+
+    private float x;
+    private float y;
+    private float rotation;
+
+    public EntityModel(float x,float y, float rotation){
+        this.x=x;
+        this.y=y;
+        this.rotation=rotation;
+    }
+
+    public float getX(){
+        return x;
+    }
+    public float getY(){
+        return y;
+    }
+    public float getRotation(){
+        return rotation;
+    }
+
+    public void setX(float x){
+        this.x=x;
+    }
+    public void setY(float y){
+        this.y=y;
+    }
+    public void setRotation(float rotation){
+        this.rotation=rotation;
+    }
+    public abstract ModelType getType();
+}
--- snake/core/src/com/snake/game/model/entities/SnakeModel.java
+++ snake/core/src/com/snake/game/model/entities/SnakeModel.java
@@ -1,4 +1,26 @@
 package com.snake.game.model.entities;
 
-public class SnakeModel {
-}
+public class SnakeModel extends EntityModel {
+    private int size;
+
+    SnakeModel(int size, float x, float y,float rotation){
+        super(x,y,rotation);
+        this.size=size;
+
+    }
+
+    @Override
+    public ModelType getType() {
+        return ModelType.SNAKE;
+    }
+    public int getSize(){
+        return size;
+    }
+    public void setSize(int size){
+        this.size=size;
+    }
+    public int updateSize(int value){
+        this.size= this.size-value;
+        return this.size;
+    }
+        }
