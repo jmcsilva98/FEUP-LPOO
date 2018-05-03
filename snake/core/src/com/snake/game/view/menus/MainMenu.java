2df5e9bf938a9e14923ce6ab012623148b2469b3
Unknown <joana.m.c.silva.pt@gmail.com>
2018-05-03 16:42:15 +0100

    Added images and altered launchers

---
+++ snake/core/src/com/snake/game/view/menus/MainMenu.java
@@ -0,0 +1,5 @@
+package com.snake.game.view.menus;
+
+
+public class MainMenu {
+}
--- snake/desktop/src/com/snake/game/desktop/DesktopLauncher.java
+++ snake/desktop/src/com/snake/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.snake.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.snake.game.Game;
+import com.snake.game.SnakeSmash;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new SnakeSmash(), config);
 	}
 }
